ae9b0128f54dd4c018f88c60abc877920c7a7a92
Joshua Gagnier <gagnierjoshua@gmail.com>
2024-01-08 22:15:51 -0800

    update background

--- background.js
+++ background.js
@@ -6,7 +6,7 @@
 //API KEY & URL
 const apiKey="zpAJWuWgF++1OeTRzDpVnQ==NUdczhqSWGHOGpvU";
 //free key with 10k calls per month :) not worried about it being public since it's free and will shut down afterwards
-const apiUrl="https://api.api-ninjas.com/v1/webscraper?url=https://example.com";
+const apiUrl="https://api.api-ninjas.com/v1/webscraper?url=";
 
 chrome.runtime.onMessage.addListener(data => {
     const { event, prefs } = data;
@@ -24,22 +24,22 @@ const handleOnStart = (prefs) => {
     console.log("onStart in background")
     console.log("prefs received:", prefs)
     chrome.storage.local.set(prefs)
-
-
-
     const url= apiUrl+prefs.inputEntry; //url is the api url plus the input entry
-    
     console.log(url);
 
-    fetch(url, { //fetching the url
-        method: "GET",
+    fetch(url, { 
+        //method: 'GET',
         headers: {  //headers for the api
             'X-API-KEY': apiKey  //api key
         }
     })
-    const data = response.json();
-    console.log(data);
-
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
 };
 
 
